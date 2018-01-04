4787c11ddd3e532bf40efceb13a7004a9f0d7704
triqadafi <mail@triqadafi.com>
2018-01-04 17:18:08 +0700

    done

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
+#define SERIAL_DELAY 5000
 #define SERIAL_RX 1
 #define SERIAL_TX 0
 #define SERIAL_BAUDRATE 9600
 #define SERIAL_RANGE 70
+
+
+#define FI_STATE_FINDING 0
+#define FI_STATE_CHECK 1
+
+#define FI_PHASE_OSCCAL_CHANGE 0
+#define FI_PHASE_OSCCAL_CHANGE_TIMEOUT 100
+#define FI_PHASE_OSCCAL_WAIT 1
+#define FI_PHASE_SERIAL_TRY 2
+#define FI_PHASE_SERIAL_WAIT 3
+#define FI_PHASE_SERIAL_WAIT_TIMEOUT 400
 
 SoftwareSerial tinySerial(SERIAL_RX, SERIAL_TX); // RX, TX
 
@@ -13,88 +25,95 @@ SoftwareSerial tinySerial(SERIAL_RX, SERIAL_TX); // RX, TX
  * -------------------------------------------*/
 char FI_USART_BUFFER[64] = {0};              // a string to hold incoming data
 bool FI_USART_RECEIVER_FLAG = false;   // whether the string is complete
-int FACTORY_OSCCAL = 0;
+int FI_OSCCAL_FACTORY = 0;
 int FI_OSCCAL_I = 0;
-int FI_OSCCAL[40] = {0};
+int FI_OSCCALS[40] = {0};
 uint8_t FI_OSCCAL_INDEX = 0;
 uint8_t FI_PHASE = 0;
-int16_t FI_I = 0;
+uint8_t FI_STATE = 0;
 int16_t FI_J = 0;
 uint32_t FI_MS = 0;
 void tinySerialEvent(void);
 
 void setup() {
   // put your setup code here, to run once:
-  FACTORY_OSCCAL = OSCCAL;
+  FI_OSCCAL_FACTORY = OSCCAL;
   OSCCAL += -1 * SERIAL_RANGE;
   FI_OSCCAL_I = -1 * SERIAL_RANGE;
-  FI_I = -1 * SERIAL_RANGE;
 
   // Start the Serial
   tinySerial.begin(SERIAL_BAUDRATE);
-  delay(5000);
+  delay(SERIAL_DELAY);
   // INIT
   FI_MS = millis();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(FI_PHASE == 0){
-    if(FI_I < SERIAL_RANGE){
+  if(FI_PHASE == FI_PHASE_OSCCAL_CHANGE){
+    if(FI_STATE == FI_STATE_FINDING){
       OSCCAL += 1;
-      FI_I += 1;
       FI_OSCCAL_I += 1;
+
+      FI_PHASE++;
       FI_MS = millis();
-      FI_PHASE++;
-    }else{
-      if(FI_OSCCAL[FI_J] != 0){
-        FI_OSCCAL_I = FI_OSCCAL[FI_J];
-        OSCCAL = FACTORY_OSCCAL + FI_OSCCAL_I;
-        FI_OSCCAL_INDEX = FI_J;
-        FI_J += 1;
-        FI_MS = millis();
-        FI_PHASE++;
-      }else{
-        while(1);
+    }else if(FI_STATE == FI_STATE_CHECK){
+      FI_OSCCAL_I = FI_OSCCALS[FI_OSCCAL_INDEX];
+      while(!FI_OSCCALS[FI_OSCCAL_INDEX]){
+        if(FI_OSCCAL_INDEX < 40){
+          FI_OSCCAL_INDEX++;
+        }else{
+          while(1); // DONE 8)
+        }
       }
+      OSCCAL = FI_OSCCAL_FACTORY + FI_OSCCAL_I;
+      FI_PHASE++;
+      FI_MS = millis();
     }
   }
-  if(FI_PHASE == 1){
-    if(millis() - FI_MS > 500){
+  if(FI_PHASE == FI_PHASE_OSCCAL_WAIT){
+    if(millis() - FI_MS > FI_PHASE_OSCCAL_CHANGE_TIMEOUT){
       FI_PHASE++;
     }
   }
-  if(FI_PHASE == 2){
+  if(FI_PHASE == FI_PHASE_SERIAL_TRY){
     tinySerial.println();
-    tinySerial.print("3913;OSCCAL += ");
-    tinySerial.print(FI_OSCCAL_I);
-    tinySerial.print(";");
-    tinySerial.print(FI_OSCCAL_INDEX);
-    tinySerial.println(";");
-    tinySerial.println("3913;ABCDEFGHIJKLMNOPQRSTUVWXYZ;");
-    FI_MS = millis();
+    tinySerial.println("3913;ABCDEFGHIJKLMNOPQRSTUVWXYZ;1;");
+    //tinySerial.println("3913;abcdefghijklmnopqrstuvwxyz;1;");
+    //tinySerial.println("3913;0123456789;1;");
     FI_PHASE++;
+    FI_MS = millis();
   }
-  if(FI_PHASE == 3){
-    if(millis() - FI_MS > 1000){
+  if(FI_PHASE == FI_PHASE_SERIAL_WAIT){
+    if(millis() - FI_MS > FI_PHASE_SERIAL_WAIT_TIMEOUT){
+      if(FI_OSCCAL_I < SERIAL_RANGE){
+        FI_STATE = FI_STATE_FINDING;
+      }else if(FI_STATE == FI_STATE_FINDING){
+        FI_STATE = FI_STATE_CHECK;
+        FI_OSCCAL_INDEX = 0;
+      }else if(FI_STATE == FI_STATE_CHECK){
+        FI_OSCCAL_INDEX++;
+      }
       FI_PHASE = 0;
     }
   }
+
+  // IF THERE IS A MIRACLE (REPLY)
   if (FI_USART_RECEIVER_FLAG) {
     FI_USART_RECEIVER_FLAG = false;
     char * item = strtok (FI_USART_BUFFER, ";"); //getting first word (uses space & comma as delimeter)
     item = strtok (NULL, ";"); //getting subsequence word
     if(!(strcmp(item,"ABCDEFGHIJKLMNOPQRSTUVWXYZ"))){
-      if(FI_I < SERIAL_RANGE){
-        FI_OSCCAL[FI_OSCCAL_INDEX] = FI_OSCCAL_I;
+      if(FI_STATE == FI_STATE_FINDING){
+        FI_OSCCALS[FI_OSCCAL_INDEX] = FI_OSCCAL_I;
         FI_OSCCAL_INDEX++;
-        tinySerial.println("3913;OK;1;");
-      }else{
-        tinySerial.print("You can use this OSCCAL += ");
+        tinySerial.print("3913;OSCCAL=");
         tinySerial.print(FI_OSCCAL_I);
-        tinySerial.print("; or OSCCAL = ");
-        tinySerial.print(FACTORY_OSCCAL + FI_OSCCAL_I);
-        tinySerial.println(";");
+        tinySerial.println(";0;");
+      }else if(FI_STATE == FI_STATE_CHECK){
+        tinySerial.print("3913;OSCCAL=");
+        tinySerial.print(FI_OSCCAL_I);
+        tinySerial.println(";1;");
       }
     }
   }
@@ -104,10 +123,10 @@ void loop() {
 
 /*
   SerialEvent occurs whenever a new data comes in the
- hardware serial RX.  This routine is run between each
- time loop() runs, so using delay inside loop can delay
- response.  Multiple bytes of data may be available.
- */
+  hardware serial RX.  This routine is run between each
+  time loop() runs, so using delay inside loop can delay
+  response.  Multiple bytes of data may be available.
+  */
 uint8_t FI_USART_BUFFER_INDEX = 0;
 void tinySerialEvent() {
   while (tinySerial.available()) {
